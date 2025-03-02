0a15a2101d8dff75a37a96855b0ec3bcc75b4f4d
Richard Duong <richardduong827@gmail.com>
2025-03-02 09:19:46 -0700

    Edit Profile and Card Template Functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,82 @@
- const initialCards = [
-     {
-        name: "Val Thorens",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
-     },
-      {
-          name: "Restaurant terrace",
-          link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
-      },
-      {
-          name: "An outdoor cafe",
-          link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
-      },
-      {
-          name: "A very long bridge, over the forest and through the trees",
-          link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
-      },
-      {
-          name: "Tunnel with morning light",
-          link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
-      },
-      {
-          name: "Mountain house",
-          link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
-      }
- ];
+/* Edit Profile Open/Close */
+const profileModal = document.querySelector('.modal');
+const profileEditButton = document.querySelector('.profile__edit-btn');
+const profileCloseButton = document.querySelector('.modal__close-btn');
 
- /* Edit Profile */
- let profileModal = document.querySelector('.modal');
- let profileEditButton = document.querySelector('.profile__edit-btn');
-let profileCloseButton = document.querySelector('.modal__close-btn');
+/* Edit Profile Name/Description */
+const profileName = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
+const profileInputName = document.querySelector('#profile_name');
+const profileInputDescription = document.querySelector('#profile_description');
 
+/* Edit Profile Form + Save Button*/
+const profileForm = document.querySelector('.modal__form');
+const profileSaveButton = document.querySelector('.modal__submit-btn');
+
+/* Open Edit Profile && Update inputs / Close Edit Profile */
 function modalOpen() {
+  profileInputName.value = profileName.textContent;
+  profileInputDescription.value = profileDescription.textContent;
   profileModal.classList.add('modal_opened');
+};
+function modalClose() { profileModal.classList.remove('modal_opened'); };
+
+/* Save Edit Profile */
+function modalSave(evt) {
+  evt.preventDefault();
+  profileName.textContent = profileInputName.value;
+  profileDescription.textContent = profileInputDescription.value;
+  modalClose();
+};
+
+/* Card Template */
+const cardTemplate = document.querySelector('#card-template');
+const cardsList = document.querySelector('.cards');
+const initialCards = [
+  {
+      name: "Val Thorens",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+  {
+      name: "Restaurant terrace",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+  },
+  {
+      name: "An outdoor cafe",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+  },
+  {
+      name: "A very long bridge, over the forest and through the trees",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+  },
+  {
+      name: "Tunnel with morning light",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+  },
+  {
+      name: "Mountain house",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+  }
+];
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.content.querySelector('.card').cloneNode(true);
+  const cardDescriptionEl = cardElement.querySelector('.card__description');
+  const cardImageEl = cardElement.querySelector('.card__image');
+
+  cardDescriptionEl.textContent = data.name;
+  cardImageEl.src = data.link;
+  cardImageEl.alt = data.name;
+
+  return cardElement;
+};
+
+for (let i = 0; i < initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards[i]);
+  cardsList.append(cardElement);
 }
 
-function modalClose() {
-  profileModal.classList.remove('modal_opened');
-}
+profileEditButton.addEventListener('click', modalOpen); // Edit Profile Open Button
+profileCloseButton.addEventListener('click', modalClose); // Edit Profile Close Button
+profileForm.addEventListener('submit', modalSave); // Edit Profile Save Button
 
-profileEditButton.addEventListener('click', modalOpen);
-
-profileCloseButton.addEventListener('click', modalClose);
